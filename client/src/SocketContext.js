6b5f7cfd3e9ae7ea27c5c53fbd24eca2901c1f7b
saikirangadupathi <141751890+saikirangadupathi@users.noreply.github.com>
2025-12-05 03:44:55 +0530

    Update SocketContext.js

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -4,7 +4,7 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-const socket = io('http://localhost:5050/');
+const socket = io('https://videochat-077p.onrender.com/');
 
 const ContextProvider = ({ children }) => {
 
